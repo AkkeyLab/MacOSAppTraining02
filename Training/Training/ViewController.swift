3a4811a891c291a8dcd8893f7a19db5123a36c16
AKIO <maizima44ww@gmail.com>
2016-08-27 23:53:09 +0900

    Read image

--- Training/Training/ViewController.swift
+++ Training/Training/ViewController.swift
@@ -9,11 +9,32 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    @IBOutlet weak var outPutImage: NSImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+
+    @IBAction func savePush(sender: AnyObject) {
+        let panel = NSOpenPanel()
+        panel.canChooseFiles = false
+        panel.canChooseDirectories = true
+        panel.allowsMultipleSelection = false
+        panel.beginWithCompletionHandler({ (num) -> Void in
+            if num == NSModalResponseOK {
+                NSLog("\(panel.directoryURL?.path)")
+
+                self.readItem((panel.directoryURL?.path)! + "/sample.png")
+            }
+        })
+
+    }
+
+    func readItem(url: String) {
+        let image: NSImage = NSImage(contentsOfFile: url)!
+        outPutImage.image = image
     }
 
     override var representedObject: AnyObject? {
